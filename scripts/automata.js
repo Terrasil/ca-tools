b89cc6c6b28429d581aea946f6eabac64a66ffd1
terrasil <gormit7@gmail.com>
2024-03-31 13:56:40 +0200

    Draw tape

--- scripts/automata.js
+++ scripts/automata.js
@@ -1,14 +1,50 @@
+let strokeColor;
+let palette;
+let cellSize = 20;
+let tapeLenght = 8;
+let tape = [0, 1, 0, 1, 0, 0, 1, 1];
+let timeLenght = 10;
+let time;
+
 function setup() {
-  // Create canvas and set its size
   const canvas = createCanvas(400, 400);
-  // Move the canvas inside the div with id 'p5-sketch'
-  canvas.parent('p5-sketch');
+  canvas.parent('automata');
+  strokeColor = color(0, 0, 0, 255);
+  cellSize = 20;
+  tapeLenght = 8;
 }
 
 function draw() {
-  // Set background color
-  background(220);
+  for (var t = 0; t < timeLenght; t++) {
+    timeRow = []
+    for (var i = 0; i < tapeLenght; i++) {
+      timeRow = []
+      drawCell(tape[i],cellSize+i*cellSize*2, cellSize+cellSize*t*2, cellSize, color(255, 204, 0, 255));
+    }
+    time.push(timeRow)
+  }
+}
+
+function calculate(D,k,T,F = null){
+  if (F === null) return standardCalucation(D,k);
+  var customFunction = new Function("D", "k", "T", F);
+  return customFunction(D,k,T);
+}
+
+function standardCalucation(D,k){
+
+}
+
+function drawCell(v, x, y, size, color){
+  strokeWeight(2);
+  stroke(strokeColor);
   
-  // Draw a simple shape (e.g., ellipse)
-  ellipse(width/2, height/2, 50, 50);
+  rectMode(RADIUS);
+  noFill();
+  rect(x, y, size, size);
+  if (v > 0){
+    rectMode(CENTER);
+    fill(color);
+    rect(x, y, size*1.5, size*1.5);
+  }
 }
