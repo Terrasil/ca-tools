307c7e8190b3a4c89419f608043f4edd55553742
terrasil <gormit7@gmail.com>
2024-05-11 16:45:36 +0200

    setup packages

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,38 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import PrimeVue from 'primevue/config'
+
+import 'primeflex/primeflex.css'
+import 'primevue/resources/themes/aura-light-blue/theme.css'
+import 'primevue/resources/primevue.min.css'
+import 'primeicons/primeicons.css'
+
+import AnimateOnScroll from 'primevue/animateonscroll'
+import BadgeDirective from 'primevue/badgedirective'
+import ConfirmationService from 'primevue/confirmationservice'
+import DialogService from 'primevue/dialogservice'
+import FocusTrap from 'primevue/focustrap'
+import Ripple from 'primevue/ripple'
+import StyleClass from 'primevue/styleclass'
+import ToastService from 'primevue/toastservice'
+import Tooltip from 'primevue/tooltip'
 
 const app = createApp(App)
 
 app.use(router)
+
+app.use(PrimeVue, { ripple: true })
+app.use(ConfirmationService)
+app.use(ToastService)
+app.use(DialogService)
+app.use(router)
+
+app.directive('tooltip', Tooltip)
+app.directive('badge', BadgeDirective)
+app.directive('ripple', Ripple)
+app.directive('styleclass', StyleClass)
+app.directive('focustrap', FocusTrap)
+app.directive('animateonscroll', AnimateOnScroll)
 
 app.mount('#app')
--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,3 +1,15 @@
-<template>test</template>
+<template>
+  <div class="card flex justify-content-center">
+    <div class="flex flex-column gap-2">
+      <label for="username">Username</label>
+      <InputText id="username" v-model="value" />
+    </div>
+  </div>
+</template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+import InputText from 'primevue/inputtext'
+
+const value = ref(null)
+</script>
